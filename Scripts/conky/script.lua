93a6c7ccfcb4a7889e40524d54c221cd539c98ce
danil <DDStyle>
2020-07-14 23:02:26 +0300

    Deleted some trash

--- Scripts/conky/script.lua
+++
@@ -1,8 +0,0 @@
-requre 'cairo'
-
-function conky_main()
-	 local updates = tonumber (conky_parse ('${updates}'))
-    if updates > 5 then
-        print ("hello world")
-    end
-end
--- Scripts/glava.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pidof glava
-if [[ $? -eq 1 ]]; then
-	glava -i
-else
-	killall glava
-
-fi
