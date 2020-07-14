2e807c5d30c52a1b7cba3a48c9f749aa00a1ffe4
danil <DDStyle>
2020-07-14 23:49:03 +0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+cp ./dotfiles/conkyrc $HOME/.conkyrc
+cp ./dotfiles/conkyrc2 $HOME/.conkyrc2
+cp ./dotfiles/conkyrc_3 $HOME/.conkyrc3
+cp ./dotfiles/zshrc $HOME/.zshrc
+
+cp -r ./dotfiles/config/glava $HOME/.config/glava
+cp -r ./dotfiles/config/kitty $HOME/.config/kitty
+cp -r ./dotfiles/config/neofetch $HOME/.config/neofetch
+cp -r ./dotfiles/config/picom $HOME/.config/picom
+cp -r ./dotfiles/config/tint2 $HOME/.config/tint2
+cp -r ./dotfiles/config/jgmenu $HOME/.config/jgmenu
+cp -r ./dotfiles/config/mpd $HOME/.config/mpd
+cp -r ./dotfiles/config/openbox $HOME/.config/openbox
+cp -r ./dotfiles/config/polybar $HOME/.config/polybar
+cp -r ./dotfiles/config/ranger $HOME/.config/ranger
+
+
+
+
